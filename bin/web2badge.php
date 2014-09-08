c68f2509b21e30eca4790d21007b4b46989ef26b
Luís Faceira <luis.faceira@beubi.com>
2014-09-08 23:24:46 +0100

    Updated messages database setup process

--- bin/web2badge.php
+++ bin/web2badge.php
@@ -1,9 +1,9 @@
 #!/usr/bin/env php
 <?php
 
-require_once __DIR__.'/vendor/autoload.php';
+$app = require_once __DIR__.'/../src/bootstrap.php';
 
-use Symfony\Component\Console\Application;
-
-$application = new Application("Web2Badge Command-Line-Interface", "0.1.0");
-$application->run();
+$cli = $app['console'];
+$cli->add(new \AveiroMakers\Web2Badge\Console\SendMessageCommand());
+$cli->add(new \AveiroMakers\Web2Badge\Console\SetupDatabaseCommand());
+$cli->run();
