31d9d2394f7436339e8d142aa1eea0b66bab977e
Luís Faceira <luis.faceira@beubi.com>
2014-09-08 23:26:14 +0100

    Added basic endpoints and updated index

--- web/index.php
+++ web/index.php
@@ -1,11 +1,39 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Request;
 
-$app = new Silex\Application();
+$app = require_once(__DIR__.'/../src/bootstrap.php');
 
-$app->get('', function() { 
-    return 'Welcome to web2badge web server'; 
+$app->get('', function() use ($app) { 
+    return $app['twig']->render('index.html');
 }); 
+
+$app->get('messages/get_one', function() use ($app) {
+    $sql = 'SELECT * FROM messages WHERE sent = ? ORDER BY timestamp ASC';
+    $message = $app['db']->fetchAssoc($sql, array(0));
+    if ($message === false)
+    {
+        return "EMPTY";
+    }
+    else
+    {
+        $sql = "UPDATE messages SET sent = ? WHERE id = ?";
+        $app['db']->executeUpdate($sql, array(1, $message['id']));
+    }
+    
+    return $message['deviceID'].$message['text'];
+});
+
+$app->post('messages', function(Request $request) use ($app) {
+    $now = new \DateTime();
+    $newMessageData = array(
+        'deviceID' => $request->get('deviceID'),
+        'text' => $request->get('text'),
+        'timestamp' => $now->format('Y-m-d H:i:s')
+    );
+
+    $app['db']->insert('messages', $newMessageData);
+    return 'OK';
+});
 
 $app->run();
