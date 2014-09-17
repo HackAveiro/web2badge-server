50f66fd4c0bae3d33ba570c4a17e3555b2e19027
Luís Faceira <luis.faceira@beubi.com>
2014-09-17 22:51:29 +0100

    Corrected get action reference of messages:getOne

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@ $app = require_once(__DIR__.'/../src/bootstrap.php');
 
 $app->get('', 'AveiroMakers\Web2Badge\Web\MainController::index');
 
-$app->get('messages/get_one', 'AveiroMakers\Web2Badge\Web\MessagesController::getOneAction');
+$app->get('messages/get_one', 'AveiroMakers\Web2Badge\Web\MessagesController::getOne');
 
 $app->get('messages', 'AveiroMakers\Web2Badge\Web\MessagesController::getAll');
 
